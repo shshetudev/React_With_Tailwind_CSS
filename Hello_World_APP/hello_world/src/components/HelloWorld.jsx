f83fe665751f0022a98d8d027e1c592d4bfe0666
shahariar.hossen <shahariar.hossen@datasoft-bd.com>
2021-05-18 20:40:18 +0600

    adding hello world

---
+++ Hello_World_APP/hello_world/src/components/HelloWorld.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function HelloWorld({msg}) {
+    return (
+        <h1 className={"text-5xl font-bold text-purple-500"}>{msg}</h1>
+    );
+}
+
+export default HelloWorld;
